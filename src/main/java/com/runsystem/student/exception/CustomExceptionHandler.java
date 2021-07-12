651fd8cf86fb5fc1c583bdd829b8804a2f40e77b
longnt1 <longnt1@runsystem.net>
2021-07-12 14:46:05 +0700

    update nameAPI and custom exception

--- src/main/java/com/runsystem/student/exception/CustomExceptionHandler.java
+++ src/main/java/com/runsystem/student/exception/CustomExceptionHandler.java
@@ -9,9 +9,25 @@ import org.springframework.web.context.request.WebRequest;
 @RestControllerAdvice
 public class CustomExceptionHandler {
 	
+	// Handle all NotFoundException 404
 	@ExceptionHandler(NotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ErrorResponse handlerNotFoundException(NotFoundException ex, WebRequest req) {
 		return new ErrorResponse(HttpStatus.NOT_FOUND ,ex.getMessage());
 	}
+	
+    @ExceptionHandler(DuplicateRecordException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handlerDuplicateRecordException(DuplicateRecordException ex, WebRequest req) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    // Handle all undeclared exceptions
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handlerException(Exception ex, WebRequest req) {
+        // Log err
+    	
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
 }
